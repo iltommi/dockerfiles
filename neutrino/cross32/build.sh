0bfb79540af88478aa138b26adadd5e4a733d28a
Tommaso Vinci <tommaso.vinci@polytechnique.edu>
2020-12-01 09:15:40 +0100

    ubuntu simplified

--- neutrino/cross32/build.sh
+++ neutrino/cross32/build.sh
@@ -6,6 +6,7 @@ tar -zxvf fftw-3.3.6-pl2.tar.gz && cd fftw-3.3.6-pl2
 mingw32-configure --disable-static --enable-shared --enable-threads --with-combined-threads 
 make -j $(nproc) bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS= 
 make install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=  
+cd ..
 
 wget https://support.hdfgroup.org/ftp/HDF/releases/HDF4.2.10/src/hdf-4.2.10.tar.bz2
 tar -jxvf hdf-4.2.10.tar.bz2 && cd hdf-4.2.10
@@ -21,13 +22,15 @@ make -C hdf/src -j $(nproc) LDFLAGS=-no-undefined
 make -C hdf/src -j 1 install
 make -C mfhdf/libsrc -j $(nproc) LDFLAGS="-no-undefined -ldf"
 make -C mfhdf/libsrc -j 1 install 
+cd ..
 
 git clone --recursive -j$(nproc) https://github.com/NeutrinoToolkit/Neutrino.git
-cd Neutrino/PythonQt
-mkdir cross && cd cross
-mingw32-cmake .. -DQt5_DIR=/usr/i686-w64-mingw32/sys-root/mingw/lib/cmake/Qt5 -DPythonQt_Wrap_QtAll=TRUE
-make -j$(nproc) install
-cd ../.. 
+# cd Neutrino/PythonQt
+# mkdir cross && cd cross
+# mingw32-cmake .. -DQt5_DIR=/usr/i686-w64-mingw32/sys-root/mingw/lib/cmake/Qt5 -DPythonQt_Wrap_QtAll=TRUE
+# make -j$(nproc) install
+# cd ../.. 
+cd Neutrino
 mkdir cross && cd cross
 mingw32-cmake ..
 make -j$(nproc) package
