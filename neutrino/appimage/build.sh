07794c9499cbb5c0bb326530a4a25b0846984385
Tommaso Vinci <tommaso.vinci@polytechnique.edu>
2022-08-03 16:40:57 +0200

    simpler appimage

--- neutrino/appimage/build.sh
+++ neutrino/appimage/build.sh
@@ -6,10 +6,10 @@ set -x
 mkdir build && cd build
 cmake -DNEUTRINO_SKIP_PLUGINS='Shell' -DCMAKE_INSTALL_PREFIX=/usr /mnt/Neutrino
 make install DESTDIR=AppDir
-wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
-chmod +x linuxdeploy*.AppImage
-export APPIMAGE_EXTRACT_AND_RUN=1
-export LD_LIBRARY_PATH=`pwd`/AppDir/usr/lib
-./linuxdeploy-x86_64.AppImage --appdir AppDir --plugin qt --output appimage -i /mnt/Neutrino/resources/icons/icon.svg --custom-apprun=/mnt/Neutrino/resources/linuxPackage/AppRun
+# wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+# wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
+# chmod +x linuxdeploy*.AppImage
+# export APPIMAGE_EXTRACT_AND_RUN=1
+# export LD_LIBRARY_PATH=`pwd`/AppDir/usr/lib
+/linuxdeploy-x86_64.AppImage --appdir AppDir --plugin qt --output appimage -i /mnt/Neutrino/resources/icons/icon.svg --custom-apprun=/mnt/Neutrino/resources/linuxPackage/AppRun
 cp Neutrino-*.AppImage /mnt
